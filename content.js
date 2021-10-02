4aded15d72aee88744aca92c7511ce1ab609943d
Weronika Żak <weronikarzak@gmail.com>
2021-10-02 21:30:36 +0100

    add pdf converting functionality

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-alert("test")
+document.addEventListener('DOMContentLoaded', function() {
+    var convertButton = document.getElementById('convertButton');
+    convertButton.addEventListener('click', function() {
+        chrome.tabs.getSelected(null, function(tab) {
+            // chrome.tabs.sendRequest(tab.id, {action: "getDOM"}, function(response) {
+            //     console.log(response.dom);
+            //   });
+            var element = document.querySelector('body');
+            html2pdf(element);
+        });
+        
+    }, false);
+}, false);
