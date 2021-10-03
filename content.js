1104b1f0f69a06f9019154d46a2b30005adb9bd3
Noted70 <bamlakkal2002@gmail.com>
2021-10-03 14:16:16 +0100

    Added average mark calc

--- content.js
+++ content.js
@@ -1,15 +1,19 @@
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+    // use application-list class to identify table
     var table = document.getElementsByClassName("application-list")[0];
-    
+    let totalCredits = 0
     var rowItems = [];
+    // populate array with table items
     for (var i = 0, row; row = table.rows[i]; i++) {
         var colItems = [];
         for (var j = 0, col; col = row.cells[j]; j++) {
+            if (j == 7 && i>0) {
+                totalCredits+= parseInt(col.textContent)
+            }
             colItems.push(col.textContent);
         }  
         rowItems.push(colItems);
     }
-
-    console.log( rowItems)
-    sendResponse({rows: rowItems})
+    
+    sendResponse({rows: rowItems, credits: totalCredits})
 })
